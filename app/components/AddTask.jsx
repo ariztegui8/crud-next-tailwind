17b849f44f0a505edc05cebe040122c45ada2080
ariztegui8 <jorgeariztegui8@gmail.com>
2023-06-24 20:27:42 -0300

    inicio de crud tailwind

---
+++ app/components/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTask = () => {
+  return (
+    <div>
+        <button className="btn">Button</button>
+    </div>
+  )
+}
+
+export default AddTask
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddTask from './components/AddTask'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <AddTask />
+    </div>
+  )
+}
+
+export default Home
